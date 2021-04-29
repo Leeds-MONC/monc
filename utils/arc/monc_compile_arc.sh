94b4b845bd1c2bf2eff16ca89c8599f16777778e
Chrisymonds <c.c.symonds@leeds.ac.uk>
2021-04-29 16:38:12 +0100

    Added compile options and keyword handling for arc4

--- utils/arc/monc_compile_arc.sh
+++ utils/arc/monc_compile_arc.sh
@@ -1,4 +1,14 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+keywrdfile=$( sed "s|= |= $( pwd -P )\/fcm-make\/|g" fcm-make/keyword.cfg )
+if [ ! -f ~/.metomi/fcm/keyword.cfg ]; then
+  mkdir -p ~/.metomi/fcm
+  echo ${keywrdfile} > ~/.metomi/fcm/keyword.cfg
+else
+  if cat ~/.metomi/fcm/keyword.cfg | grep -q -v "${keywrdfile}"; then
+    echo ${keywrdfile} >> ~/.metomi/fcm/keyword.cfg
+  fi
+fi
 
 #Script to compile Monc on ARC4:
 
@@ -7,5 +17,31 @@ module load user
 module switch intel gnu
 module switch openmpi mvapich2
 module load fftw netcdf hdf5 fcm
+. /nobackup/cemac/cemac.sh
 
-fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg
+echo "Compile options: "
+echo "(1)   MONC Standalone,"
+echo "(2)   MONC with CASIM,"
+echo "(3)   MONC with SOCRATES,"
+echo "(4)   MONC with CASIM and SOCRATES"
+echo ""
+echo "Select which option [1-4]: "
+read compileoption
+
+case $compileoption in
+1)
+  fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg
+  ;;
+2)
+  fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg -f fcm-make/casim.cfg
+  ;;
+3)
+  fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg -f fcm-make/socrates.cfg
+  ;;
+4)
+  fcm make -j4 -f fcm-make/monc-arc4-gnu.cfg -f fcm-make/casim_socrates.cfg
+  ;;
+*)
+  echo "Unexpected compilation option. Should be an integer in the range 1-4"
+  ;;
+esac
